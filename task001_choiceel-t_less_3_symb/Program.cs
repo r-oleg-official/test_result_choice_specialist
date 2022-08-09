6130e2844d56dd0951e063f7e80a3032a0b8234a
Oleg Razumov <r.oleg.official@gmail.com>
2022-08-09 23:06:07 +0300

    First version the solution.

--- task001_choiceel-t_less_3_symb/Program.cs
+++ task001_choiceel-t_less_3_symb/Program.cs
@@ -11,15 +11,41 @@
 
 Console.Clear();
 string[] symbolsArray = {"hello", "2", "world", ":-)"};
+// string[] symbolsArray = {"1234", "1567", "-2", "computer science"};
+// string[] symbolsArray = {"Russia", "Denmark", "Kazan"};
+string[] threeSymbols = new string[FindCountThreeSymbolsElement(symbolsArray)];
+
+int index = 0;
+for (int i = 0; i < symbolsArray.Length; i++)
+{
+    if (symbolsArray[i].Length <= 3) 
+    {
+        threeSymbols[index] = symbolsArray[i];
+        index++;
+    }
+}
+
 PrintStringArray(symbolsArray);
-
-
+Console.Write(" -> ");
+PrintStringArray(threeSymbols);
+Console.WriteLine();
 
 void PrintStringArray(string[] array)
 {
+    Console.Write("[");
     for (int i = 0; i < array.Length - 1; i++)
     {
         Console.Write($"\"{array[i]}\", ");
     }
-    Console.WriteLine($"\"{array[array.Length - 1]}\"");
+    Console.Write($"\"{array[array.Length - 1]}\"]");
+}
+
+int FindCountThreeSymbolsElement(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) count++;
+    }
+    return count;
 }
